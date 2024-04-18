c7f7de2eb078803bdf46ba94f2233cd6d57b8087
Franky464 <36291976+Franky464@users.noreply.github.com>
2024-04-18 18:51:43 +0200

    Update Parameters.cpp
    added parameters in .param file

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -381,6 +381,25 @@ const AP_Param::Info Copter::var_info[] = {
     // @Values: 0:No repositioning, 1:Repositioning
     // @User: Advanced
     GSCALAR(land_repositioning, "LAND_REPOSITION",     LAND_REPOSITION_DEFAULT),
+	
+	
+	////////////// ADDED BY FRANKY
+	// @Param: LAND_DET_RNGFND
+    // @DisplayName: land detector options
+    // @Description: Defines id land detection uses lidar or acceleration
+    // @Units: 
+    // @Range: 0 1
+    // @User: Advanced
+	GSCALAR(land_detector_rngfnd, "LAND_DET_RNGFND", LAND_DETECTOR_RNGFND_DEFAULT),
+ 
+	// @Param: LAND_DET_MOT_LOW
+    // @DisplayName: land detector motor low
+    // @Description: If using a RNGFND this value is used instead Mot_at_lower_limit a condition to trigg Land Detection
+    // @Range: 0 0.15
+    // @User: Advanced
+	GSCALAR(land_detector_mot_low, "LAND_DET_MOT_LOW", LAND_DETECTOR_MOT_LOW_DEFAULT),
+	//////////////// ADDED BY FRANKY
+
 
     // @Param: FS_EKF_ACTION
     // @DisplayName: EKF Failsafe Action
