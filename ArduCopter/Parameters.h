55dd58b4752cf64c4f0143915f298a6074897dd1
Franky464 <36291976+Franky464@users.noreply.github.com>
2024-04-18 18:48:17 +0200

    Update Parameters.h
    parameters added k_param_land_detector_rngfnd 
    /switch to regular land detetion mode and big prop's mode.
    & k_param_land_detector_mot_low
    //If using a RNGFND this value is added on Mot_at_lower_limit a condition to trigg Land Detection

--- ArduCopter/Parameters.h
+++ ArduCopter/Parameters.h
@@ -382,6 +382,14 @@ public:
         k_param_throw_altitude_min,
         k_param_throw_altitude_max,
 
+
+//ADDED BY FRANKY/////////////////////////////////
+		k_param_land_detector_rngfnd, //switch to regular land detetion mode and big prop's mode
+		k_param_land_detector_mot_low, //If using a RNGFND this value is added on Mot_at_lower_limit a condition to trigg Land Detection
+//ADDED BY FRANKY/////////////////////////////////
+
+
+
         // the k_param_* space is 9-bits in size
         // 511: reserved
     };
@@ -458,6 +466,12 @@ public:
     AP_Int8         fs_crash_check;
     AP_Float        fs_ekf_thresh;
     AP_Int16        gcs_pid_mask;
+	
+	//ADDED BY FRANKY//////////////////
+	AP_Int8			land_detector_rngfnd;
+	AP_Float		land_detector_mot_low;
+    // ADDED BY FRANKY <
+
 
 #if MODE_THROW_ENABLED == ENABLED
     AP_Enum<ModeThrow::PreThrowMotorState>         throw_motor_start;
